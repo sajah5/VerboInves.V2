9448f43a8688981d4ab1a11e6bec3137b6efe0ae
Saja Al-Hayek <sajalhayek5482@gmail.com>
2024-11-15 21:29:52 +0300

    Resolved conflicts and merged changes

--- WinApp/Properties/Resources.Designer.cs
+++ WinApp/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WinApp.Properties {
+namespace VerboInves.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace WinApp.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WinApp.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("VerboInves.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
